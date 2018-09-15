77a879d8614f98304ca529804895a93385e00a46
jakeneels <jake@homeeondemand.com>
2018-09-15 18:55:39 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const request = require('request');
+const fakeUa = require('fake-useragent');
+const cheerio = require('cheerio');
+// const $ = cheerio.load('');
+
+// $('h2.title').text('Hello there!');
+// $('h2').addClass('welcome');
+//
+// $.html();
+
+let url = 'https://www.winstonslab.com/teams/team.php?id=334';
+let url1 = 'https://www.overbuff.com/esports'; //https://masteroverwatch.com/news/107-how-to-pick-your-overwatch-league-team
+let url2 = 'https://overwatchleague.com/en-us/stats'; // you can change to EU and asia i presume
+let url3 = 'https://overwatchleague.com/en-us/players/4652/ark';// recent matches
+let verb = 'get';
+
+let params = {
+  url: 'https://www.overbuff.com/heroes',
+  headers: fakeUa()
+};
+
+request.get(url, (err, req, body) => { //todo fake the header
+  console.log(body);
+  // console.log(req);
+});
