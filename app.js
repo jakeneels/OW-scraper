574c0a5d75fc138b1ed0f60cda8244963f385b23
jakeneels <jake@homeeondemand.com>
2018-09-15 21:57:48 -0400

    implement pupeteer to get ajax content, created scrape example for populating a player from OL.com/players https://overwatchleague.com/en-us/players/4142/agilities

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const request = require('request');
 const fakeUa = require('fake-useragent');
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+
+
 // const $ = cheerio.load('');
 
 // $('h2.title').text('Hello there!');
@@ -15,12 +18,25 @@ let url2 = 'https://overwatchleague.com/en-us/stats'; // you can change to EU an
 let url3 = 'https://overwatchleague.com/en-us/players/4652/ark';// recent matches
 let verb = 'get';
 
-let params = {
-  url: 'https://www.overbuff.com/heroes',
-  headers: fakeUa()
-};
+async function getPic() {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.waitFor(3000);
+  await page.click('.Button--secondary');
+  await page.waitFor(2000);
+  console.log(await page.content());
+  // await page.screenshot({path: 'google.png'});
+  
+  await browser.close();
+}
 
-request.get(url, (err, req, body) => { //todo fake the header
-  console.log(body);
-  // console.log(req);
-});
+getPic();
+
+
+
+//
+// request.get(params, (err, req, body) => { //todo fake the header
+//   console.log(body);
+//   // console.log(req);
+// });
