0358dd7a23465e2ed4ca6e29ed11cc6bf3234889
jakeneels <jake@homeeondemand.com>
2018-09-15 21:02:03 -0400

    team.js object

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const cheerio = require('cheerio');
 
 // $('h2.title').text('Hello there!');
 // $('h2').addClass('welcome');
-//
+
 // $.html();
 
+let team = require('./models/team.js');
+console.log(team);
 let url = 'https://www.winstonslab.com/teams/team.php?id=337';
 let url1 = 'https://www.overbuff.com/esports'; //https://masteroverwatch.com/news/107-how-to-pick-your-overwatch-league-team
 let url2 = 'https://overwatchleague.com/en-us/stats'; // you can change to EU and asia i presume
@@ -19,8 +21,8 @@ let params = {
   url: 'https://www.overbuff.com/heroes',
   headers: fakeUa()
 };
-
-request.get(url, (err, req, body) => { //todo fake the header
-  console.log(body);
-  // console.log(req);
-});
+//
+// request.get(url, (err, req, body) => { //todo fake the header
+//   console.log(body);
+//   // console.log(req);
+// });
