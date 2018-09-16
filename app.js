bd34b3f12f45e585dbbb5bb2b7be8f60f2c76de8
jakeneels <jake@homeeondemand.com>
2018-09-16 19:20:01 -0400

    MVP player population

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-
 const cheerio = require('cheerio');
 //const puppeteer = require('puppeteer');
 
 
-let team = require('./models/team.js');
+// let team = require('./models/team.js');
 let player = require('./models/player.js');
 let url = 'https://overwatchleague.com/en-us/players/4142/agilities';
 let url1 = 'https://www.overbuff.com/esports'; //https://masteroverwatch.com/news/107-how-to-pick-your-overwatch-league-team
@@ -41,22 +40,53 @@ let uTextRight = '.u-text-right';
 let selNames = '.Table-data--extended';
 let selTimePlayed = `.u-text-right:not(${statsOverviewTitle}):not(${selNames}):not(${percentPlayed})`;
 let selPercentPlayed = `.Table-data--emphasized:not(${statsOverviewTitle})`;
-let player = {};
 
+
+let charArr = [];
+let tempChar = {};
 let charNames = scrapeCharsData(selNames);
-charNames.forEach((name)=>{
-  player.chars[name];
+let timePlayed = scrapeCharsData(selTimePlayed, 's');
+let percentPlayedstat = scrapeCharsData(selPercentPlayed, '%');
+
+
+for (let i = 0; i < charNames.length; i++) {
+  charArr.push({
+    name: charNames[i],
+    timePlayed: timePlayed[i],
+    playPercentage: percentPlayedstat[i]
+  });
+  
+  // console.log(charArr)
+}
+for (let key in player.stats.chars) {
+  // console.log(key);
+  charArr.forEach((char) => {
+    if (key == char.name) {
+      player.stats.chars[key].timePlayed = char.timePlayed;
+      player.stats.chars[key].playPercentage = char.playPercentage;
+    }
+  })
+}
+console.log(player.stats.chars);
+console.log(('25m 7s' < '0h 0m 0s'));
+
+
+
+timePlayed.forEach((time) => {
+  Object.keys(player.stats.chars).forEach((charName) => {
+  
+  });
 });
-scrapeCharsData(selTimePlayed, 's');
-scrapeCharsData(selPercentPlayed, '%');
+
 
 function scrapeCharsData(selector, onlyIncluding) {
   let result = [];
   $(selector).each(function (i, e) {
     let content = e.children[0].data.toLowerCase();
     if (onlyIncluding != null) {
-      if(content.includes(onlyIncluding))
-      {result.push(content);}
+      if (content.includes(onlyIncluding)) {
+        result.push(content);
+      }
     } else {
       result.push(content);
     }
